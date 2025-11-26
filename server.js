dbcea690a9b805af4b08070ed66144febcc94ae9
artyomdev250 <artyom.dev250@gmail.com>
2025-11-26 23:11:02 +0500

    signup route

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const connectDB = require('./mongodb/connect');
+
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+connectDB();
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+    res.send('API running...');
 });
+
+const PORT = 3000;
+app.listen(PORT, () =>
+    console.log(`Server running at http://localhost:${PORT}`)
+);
