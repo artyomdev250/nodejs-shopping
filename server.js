3218a213c2908603abc6d11bbb01fba03fe142cc
artyomdev250 <artyom.dev250@gmail.com>
2025-11-29 23:55:51 +0500

    CORS

--- server.js
+++ server.js
@@ -9,18 +9,7 @@ const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
-const corsOptions = {
-    origin: [
-        "http://localhost:5173",
-        "https://nodejs-shopping.vercel.app/",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.use(cors());
 
 connectDB();
 app.use(express.json());
