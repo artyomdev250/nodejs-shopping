0c0a75c4912acf550c120c8fbda8c6e31ea66446
artyomdev250 <artyom.dev250@gmail.com>
2025-11-30 01:48:31 +0500

    CORS local host

--- server.js
+++ server.js
@@ -9,19 +9,26 @@ const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+    origin: "http://localhost:5173",
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 connectDB();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Hello from Node server!');
+app.get("/", (req, res) => {
+    res.send("Hello from Node server!");
 });
 
-app.listen(process.env.PORT, () =>
-    console.log(`Server running on port ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
