d4164712c937764636ac81ac459bea34a7416d4f
artyomdev250 <artyom.dev250@gmail.com>
2025-11-30 02:05:14 +0500

    Inception

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./mongodb/connect");
 
@@ -9,15 +8,7 @@ const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
-const corsOptions = {
-    origin: "http://localhost:5173",
-    credentials: true,
-};
-
-app.use(cors(corsOptions));
-
 connectDB();
-
 app.use(express.json());
 app.use(cookieParser());
 
@@ -28,7 +19,6 @@ app.get("/", (req, res) => {
     res.send("Hello from Node server!");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(process.env.PORT, () =>
+    console.log(`Server running on port ${process.env.PORT}`)
+);
