e3dcff0ce9929258d83b2946a380e958e54d3a9b
artyomdev250 <artyom.dev250@gmail.com>
2025-11-30 17:11:44 +0500

    new setup for CORS branch

--- controllers/auth/signOutController.js
+++ controllers/auth/signOutController.js
@@ -1,4 +1,5 @@
 const BlacklistedToken = require("../../mongodb/models/BlacklistedToken");
+const isProd = process.env.NODE_ENV === "production";
 
 exports.logout = async (req, res) => {
     try {
@@ -8,18 +9,16 @@ exports.logout = async (req, res) => {
             return res.status(200).json({ message: "Logged out" });
         }
 
-        // Add token to blacklist
         await BlacklistedToken.create({
             token,
             expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
         });
 
-        // Clear cookie
         res.clearCookie("refreshToken", {
             httpOnly: true,
-            secure: false,
-            sameSite: "strict",
-            path: "/"
+            secure: isProd,
+            sameSite: isProd ? "none" : "lax",
+            path: "/",
         });
 
         return res.status(200).json({ message: "Logout successful" });
